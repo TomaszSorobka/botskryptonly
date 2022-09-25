1e455db8535eebd84cacc7aaf1bab6ae26ff7855
TomaszSorobka <tomek.sorobka@gmail.com>
2022-09-25 17:08:09 +0200

    headless to true

--- bot2.js
+++ bot2.js
@@ -14,7 +14,7 @@ var pool = mysql.createPool({
 
 (async () => {
   let errorcatcher = 1;
-  const browser = await puppeteer.launch({headless: false, args: ["--no-sandbox"]});
+  const browser = await puppeteer.launch({headless: true, args: ["--no-sandbox"]});
   const page = await browser.newPage();
   await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36');
   await page.setViewport({ width: 1280, height: 720 });
